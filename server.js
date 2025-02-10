17a924bd6749a28369ac62b5c9f0f0b659a4c82d
rana <rana.buddy@gmail.com>
2025-02-10 12:00:19 +0600

    feat:first commit

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 import express from 'express';
+import cors from 'cors'
 import TagRouter from './routers/TagRouter.js';
 import connectDB from './database/db.js';
 const app = express();
 const port = 8080;
-
+app.use(cors())
 
 //call db cinnection
-connectDB();
 // Middleware to parse JSON requests
 app.use(express.json());
+connectDB();
 
 // Basic route
 app.get('/', (req, res) => {
